75bf104098b65c2903609be4ac58ca20f95956e6
Michaela Hunt <mhunt@theprovatogroup.com>
2024-09-18 15:09:01 -0400

    Modal opens; added animation too

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,16 @@
 $(function () {
     $("#cartModalDiv").load("cart.html");
     $("#couponModalDiv").load("coupon.html");
+
+    function showCouponUponLoad() {
+        //set interval 
+        // let counter = 1;
+        const couponInterval = setInterval(function () {
+            $("#couponModalDiv").modal('show');
+            clearInterval(couponInterval);
+        }, 2500);
+    }
+    
+    showCouponUponLoad();
 });
 
